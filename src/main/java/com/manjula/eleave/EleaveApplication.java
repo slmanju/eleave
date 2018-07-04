a513d25bc907ee1f95035afecbe47436b756a8fc
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-07-04 11:23:43 +0530

    initial commit

---
+++ src/main/java/com/manjula/eleave/EleaveApplication.java
@@ -0,0 +1,12 @@
+package com.manjula.eleave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleaveApplication.class, args);
+	}
+}
